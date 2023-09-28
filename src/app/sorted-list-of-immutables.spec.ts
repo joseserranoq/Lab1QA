c69601fc54d512e803daf2c24a63966e2266dc43
Jose Serrano Quesada <joseserranoq00@gmail.com>
2023-09-27 19:08:19 -0600

    clases subidas

---
+++ src/app/sorted-list-of-immutables.spec.ts
@@ -0,0 +1,7 @@
+import { SortedListOfImmutables } from './sorted-list-of-immutables';
+
+describe('SortedListOfImmutables', () => {
+  it('should create an instance', () => {
+    expect(new SortedListOfImmutables()).toBeTruthy();
+  });
+});
