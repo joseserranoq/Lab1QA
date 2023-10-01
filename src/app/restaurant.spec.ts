e913be2ee69f701c5bd0ea7dacd903a04368f0b9
Josué Quesada <josuequesadaxd@gmail.com>
2023-09-30 22:43:44 -0600

    update

--- src/app/restaurant.spec.ts
+++ src/app/restaurant.spec.ts
@@ -4,96 +4,128 @@ import { SortedListOfImmutables } from './sorted-list-of-immutables';
 import { Listable } from './listable';
 import { Entree } from './entree';
 
- 
+/*Casos de prueba para addShipmentToInventory, en el caso 1, se espera que
+se pueda añadir una lista de items al inventario del restaurante si la suma 
+de los precios de los items no es mayor al dinero disponible en el restaurante, 
+se probara utilizando una lista de comidas y se espera que retorne True, 
+para el caso 2, es similar al primero pero en este el monto de la suma excede
+el dinero del restaurante, por lo que se espera que retorne false.*/
+describe('SE PRUEBA addShipmentToInventory Y SE VERIFICA MEDIANTE UN BOOLEANO', function () {
+  let restaurante: Restaurant;
+  let listaComida: Listable[];
+  let lista2: Listable[];
+  beforeEach(function () {
+    restaurante = new Restaurant('Test', 700);
+  });
+  afterEach(function () {
+    listaComida = [];
+    lista2 = [];
+  });
 
-//CASOS DE PRUEBA DE addShipmentToInventory
-describe("SE PRUEBA addShipmentToInventory Y SE VERIFICA MEDIANTE UN BOOLEANO", function() {
-    let restaurante : Restaurant;
-    beforeEach(function() {
-        restaurante = new Restaurant("Test", 700);
+  it('1.Debe de retornar True', function () {
+    let listaComida: Listable[];
+    let lista2 = new SortedListOfImmutables(null);
+    listaComida = [
+      new Food('Bacon', 89, 185, 'Bacon.jpg'),
+      new Food('Waffle', 178, 350, 'Waffle.jpg'),
+      new Food('Egg', 47, 89, 'Egg.jpg'),
+      new Food('Orange Juice', 77, 199, 'OrangeJuice.jpg'),
+      new Food('Milk', 52, 179, 'Milk.jpg'),
+      new Food('Toast', 66, 125, 'Toast.jpg'),
+      new Food('Hashbrowns', 127, 195, 'Hashbrowns.jpg'),
+    ];
+
+    listaComida.map((comida) => {
+      lista2.add(comida);
     });
 
-    it("1.Debe de retornar True", function() {   
-        let listaComida: Listable[];
-        let lista2 = new SortedListOfImmutables(null);
-        listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
-        new Food("Egg", 47, 89, "Egg.jpg"),
-        new Food("Orange Juice", 77, 199, "OrangeJuice.jpg"),
-        new Food("Milk", 52, 179, "Milk.jpg"),
-        new Food("Toast", 66, 125, "Toast.jpg"),
-        new Food("Hashbrowns", 127, 195, "Hashbrowns.jpg")];
-    
-        listaComida.map((comida)=> {
-            lista2.add(comida);
-        });
-        
-        expect(restaurante.addShipmentToInventory(lista2)).toBeTruthy();
-    });
+    expect(restaurante.addShipmentToInventory(lista2)).toBeTruthy();
+  });
 
-    it("2.Debe retornar False", function() {
-        let listaComida: Listable[];
-        let lista2 = new SortedListOfImmutables(null);
-        listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
-        new Food("Egg", 47, 89, "Egg.jpg"),
-        new Food("Orange Juice", 77, 199, "OrangeJuice.jpg"),
-        new Food("Milk", 52, 179, "Milk.jpg"),
-        new Food("Toast", 66, 125, "Toast.jpg"),
-        new Food("Hashbrowns", 127, 195, "Hashbrowns.jpg"),new Food("Pie", 195, 275, "Pie.jpg")];
-    
-        listaComida.map((comida)=> {
-            lista2.add(comida);
-        });
-        // restaurante.addShipmentToInventory(lista2);
-        // expect(restaurante.getInventory().getSize()).toBe(7);
-        expect(restaurante.addShipmentToInventory(lista2)).toBeFalsy();
-    });
+  it('2.Debe retornar False', function () {
+    let listaComida: Listable[];
+    let lista2 = new SortedListOfImmutables(null);
+    listaComida = [
+      new Food('Bacon', 89, 185, 'Bacon.jpg'),
+      new Food('Waffle', 178, 350, 'Waffle.jpg'),
+      new Food('Egg', 47, 89, 'Egg.jpg'),
+      new Food('Orange Juice', 77, 199, 'OrangeJuice.jpg'),
+      new Food('Milk', 52, 179, 'Milk.jpg'),
+      new Food('Toast', 66, 125, 'Toast.jpg'),
+      new Food('Hashbrowns', 127, 195, 'Hashbrowns.jpg'),
+      new Food('Pie', 195, 275, 'Pie.jpg'),
+    ];
 
+    listaComida.map((comida) => {
+      lista2.add(comida);
+    });
+    expect(restaurante.addShipmentToInventory(lista2)).toBeFalsy();
+  });
 });
 
-describe("SE PRUEBA placeOrder Y SE VERIFICA MEDIANTE UN BOOLEANO", function() {
-    let restaurante : Restaurant;
-    let listaComida : Listable[];
-    let listaComida2 : Listable[];
-    let entree: Entree;
-    let lista2 : SortedListOfImmutables;
-    let lista3 : SortedListOfImmutables;
+/*Casos de prueba para placeOrder, en el caso 3, se espera que
+se pueda realizar el pedido de una entree tomando en cuenta el inventario
+disponible en el restaurante, se espera que retorne True, ya que el pedido 
+si existe en el inventario, para el caso 4 , es similar al tercero pero 
+en este parte del entree, no existe en el restaurante por lo que se espera 
+que retorne false.*/
+describe('SE PRUEBA placeOrder Y SE VERIFICA MEDIANTE UN BOOLEANO', function () {
+  let restaurante: Restaurant;
+  let listaComida: Listable[];
+  let listaComida2: Listable[];
+  let entree: Entree;
+  let lista2: SortedListOfImmutables;
+  let lista3: SortedListOfImmutables;
 
-    beforeEach(function() {
-        lista2 = new SortedListOfImmutables(null);
-        lista3 = new SortedListOfImmutables(null);
+  beforeEach(function () {
+    lista2 = new SortedListOfImmutables(null);
+    lista3 = new SortedListOfImmutables(null);
+  });
+  afterEach(function () {
+    listaComida = [];
+    listaComida2 = [];
+  });
+  it('3.Debe retornar True', function () {
+    restaurante = new Restaurant('Test', 700);
+    listaComida = [
+      new Food('Bacon', 89, 185, 'Bacon.jpg'),
+      new Food('Waffle', 178, 350, 'Waffle.jpg'),
+      new Food('Egg', 47, 89, 'Egg.jpg'),
+    ];
+
+    listaComida.map((comida) => {
+      lista2.add(comida);
     });
-    it("3.DEBE RETORNAR true", function() {
-        restaurante = new Restaurant("Test", 700);
-        listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
-        new Food("Egg", 47, 89, "Egg.jpg")];
-    
-        listaComida.map((comida)=> {
-            lista2.add(comida);
-        });
-        restaurante.addShipmentToInventory(lista2);
-        listaComida2 = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg")]
-        listaComida2.map((comida2)=> {
-            lista3.add(comida2);
-        });
-        entree = new Entree('tests', lista3);
-        console.log(entree.getFoodList());
-        expect(restaurante.placeOrder(entree)).toBeTruthy();
+    restaurante.addShipmentToInventory(lista2);
+    listaComida2 = [
+      new Food('Bacon', 89, 185, 'Bacon.jpg'),
+      new Food('Waffle', 178, 350, 'Waffle.jpg'),
+    ];
+    listaComida2.map((comida2) => {
+      lista3.add(comida2);
     });
-    it("4.DEBE DE RETORNAR false", function() {
-        restaurante = new Restaurant("Test", 700);
-        listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
-        new Food("Egg", 47, 89, "Egg.jpg")];    
-        listaComida.map((comida)=> {
-            lista2.add(comida);
-        });
-        restaurante.addShipmentToInventory(lista2);
-        listaComida2 = [new Food("Pie", 195, 275, "Pie.jpg"),
-        new Food("Croissant", 106, 125, "Croissant.jpg")];
-        listaComida2.map((comida2)=> {
-            lista3.add(comida2);
-        });
-        entree = new Entree('tests', lista3);
-        console.log(entree.getFoodList());
-        expect(restaurante.placeOrder(entree)).toBeFalsy();
+    entree = new Entree('tests', lista3);
+    expect(restaurante.placeOrder(entree)).toBeTruthy();
+  });
+  it('4.Debe retornar False', function () {
+    restaurante = new Restaurant('Test', 700);
+    listaComida = [
+      new Food('Bacon', 89, 185, 'Bacon.jpg'),
+      new Food('Waffle', 178, 350, 'Waffle.jpg'),
+      new Food('Egg', 47, 89, 'Egg.jpg'),
+    ];
+    listaComida.map((comida) => {
+      lista2.add(comida);
     });
+    restaurante.addShipmentToInventory(lista2);
+    listaComida2 = [
+      new Food('Pie', 195, 275, 'Pie.jpg'),
+      new Food('Croissant', 106, 125, 'Croissant.jpg'),
+    ];
+    listaComida2.map((comida2) => {
+      lista3.add(comida2);
     });
+    entree = new Entree('tests', lista3);
+    expect(restaurante.placeOrder(entree)).toBeFalsy();
+  });
+});
--- src/app/sorted-list-of-immutables.spec.ts
+++ src/app/sorted-list-of-immutables.spec.ts
@@ -2,36 +2,41 @@ import { Food } from './food';
 import { Listable } from './listable';
 import { SortedListOfImmutables } from './sorted-list-of-immutables';
 
-describe("SE PRUEBA getRetailValue() Y SE VERIFICA MEDIANTE LA SUMA DE UN NUMERO", function() {
-    it("1.Debe de mostrar 535", function() {   
-        let listaComida: Listable[];
-        let listaComida2: Listable[];
-        let listaComida3: Listable[];
-        let lista1 = new SortedListOfImmutables(null);
-        let lista2 = new SortedListOfImmutables(null);
-        let lista3 = new SortedListOfImmutables(null);
-        listaComida = [];
-        listaComida2 = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg")];
-        listaComida3 = [new Food("Cereal", 129, 275, "Cereal.jpg"),
-        new Food("Donut", 89, 129, "Donut.jpg"),
-        new Food("Melon", 98, 159, "Melon.jpg"),
-        new Food("Pie", 195, 275, "Pie.jpg"),
-        new Food("Croissant", 106, 125, "Croissant.jpg")]
-        listaComida.map((comida)=> {
-            lista1.add(comida);
-        });
-        listaComida2.map((comida)=> {
-            lista2.add(comida);
-        });
-        listaComida3.map((comida)=> {
-            lista3.add(comida);
-        });
-        [
-            lista1,lista2,lista3
-        ].forEach((lista) => {
-        let total = lista.getRetailValue();
-        expect(lista.getRetailValue()).toBe(total);
-        });
-    
+/*Caso de prueba para getRetailValue, donde se busca la suma total de los
+precios de los items en una lista, se espera la suma de estos valores como 
+resultado final*/
+describe('SE PRUEBA getRetailValue() Y SE VERIFICA MEDIANTE LA SUMA DE UN NUMERO', function () {
+  it('1.Debe de mostrar la suma de los precios de los items en la lista', function () {
+    let listaComida: Listable[];
+    let listaComida2: Listable[];
+    let listaComida3: Listable[];
+    let lista1 = new SortedListOfImmutables(null);
+    let lista2 = new SortedListOfImmutables(null);
+    let lista3 = new SortedListOfImmutables(null);
+    listaComida = [];
+    listaComida2 = [
+      new Food('Bacon', 89, 185, 'Bacon.jpg'),
+      new Food('Waffle', 178, 350, 'Waffle.jpg'),
+    ];
+    listaComida3 = [
+      new Food('Cereal', 129, 275, 'Cereal.jpg'),
+      new Food('Donut', 89, 129, 'Donut.jpg'),
+      new Food('Melon', 98, 159, 'Melon.jpg'),
+      new Food('Pie', 195, 275, 'Pie.jpg'),
+      new Food('Croissant', 106, 125, 'Croissant.jpg'),
+    ];
+    listaComida.map((comida) => {
+      lista1.add(comida);
     });
+    listaComida2.map((comida) => {
+      lista2.add(comida);
+    });
+    listaComida3.map((comida) => {
+      lista3.add(comida);
+    });
+    [lista1, lista2, lista3].forEach((lista) => {
+      let total = lista.getRetailValue();
+      expect(lista.getRetailValue()).toBe(total);
+    });
+  });
 });
