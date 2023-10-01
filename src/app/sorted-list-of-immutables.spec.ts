b1627a9a3534a1abfabb8afd4c95ec55917e2090
Jose Serrano Quesada <joseserranoq00@gmail.com>
2023-09-30 21:20:29 -0600

    tercera prueba parametrizada hecha

--- src/app/sorted-list-of-immutables.spec.ts
+++ src/app/sorted-list-of-immutables.spec.ts
@@ -1,7 +1,37 @@
+import { Food } from './food';
+import { Listable } from './listable';
 import { SortedListOfImmutables } from './sorted-list-of-immutables';
 
-// describe('SortedListOfImmutables', () => {
-//   it('should create an instance', () => {
-//     expect(new SortedListOfImmutables()).toBeTruthy();
-//   });
-// });
+describe("SE PRUEBA getRetailValue() Y SE VERIFICA MEDIANTE LA SUMA DE UN NUMERO", function() {
+    it("1.Debe de mostrar 535", function() {   
+        let listaComida: Listable[];
+        let listaComida2: Listable[];
+        let listaComida3: Listable[];
+        let lista1 = new SortedListOfImmutables(null);
+        let lista2 = new SortedListOfImmutables(null);
+        let lista3 = new SortedListOfImmutables(null);
+        listaComida = [];
+        listaComida2 = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg")];
+        listaComida3 = [new Food("Cereal", 129, 275, "Cereal.jpg"),
+        new Food("Donut", 89, 129, "Donut.jpg"),
+        new Food("Melon", 98, 159, "Melon.jpg"),
+        new Food("Pie", 195, 275, "Pie.jpg"),
+        new Food("Croissant", 106, 125, "Croissant.jpg")]
+        listaComida.map((comida)=> {
+            lista1.add(comida);
+        });
+        listaComida2.map((comida)=> {
+            lista2.add(comida);
+        });
+        listaComida3.map((comida)=> {
+            lista3.add(comida);
+        });
+        [
+            lista1,lista2,lista3
+        ].forEach((lista) => {
+        let total = lista.getRetailValue();
+        expect(lista.getRetailValue()).toBe(total);
+        });
+    
+    });
+});
