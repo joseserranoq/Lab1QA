88fcc7fa6303fe3a1069caa9a5c8eae7b7f9aa2a
Jose Serrano Quesada <joseserranoq00@gmail.com>
2023-09-30 20:59:44 -0600

    segunda prueba hecha

--- src/app/restaurant.spec.ts
+++ src/app/restaurant.spec.ts
@@ -7,7 +7,7 @@ import { Entree } from './entree';
  
 
 //CASOS DE PRUEBA DE addShipmentToInventory
-describe("SE AGREGA UNA LISTA A LA FUNCION addShipmentToInventory Y SE VERIFICA MEDIANTE UN BOOLEANO", function() {
+describe("SE PRUEBA addShipmentToInventory Y SE VERIFICA MEDIANTE UN BOOLEANO", function() {
     let restaurante : Restaurant;
     beforeEach(function() {
         restaurante = new Restaurant("Test", 700);
@@ -50,55 +50,50 @@ describe("SE AGREGA UNA LISTA A LA FUNCION addShipmentToInventory Y SE VERIFICA 
 
 });
 
-describe("SE AGREGA UNA LISTA A LA FUNCION placeOrder Y SE VERIFICA MEDIANTE UN BOOLEANO", function() {
+describe("SE PRUEBA placeOrder Y SE VERIFICA MEDIANTE UN BOOLEANO", function() {
     let restaurante : Restaurant;
     let listaComida : Listable[];
     let listaComida2 : Listable[];
     let entree: Entree;
     let lista2 : SortedListOfImmutables;
-    let lista3 :SortedListOfImmutables;
+    let lista3 : SortedListOfImmutables;
+
     beforeEach(function() {
-        let listaComida: Listable[];
-        let listaComida2: Listable[];
-        let lista2 = new SortedListOfImmutables(null);
-        let lista3 = new SortedListOfImmutables(null);
-
-        // listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
-        // new Food("Egg", 47, 89, "Egg.jpg")]
-    
-        // listaComida.map((comida)=> {
-        //     lista2.add(comida);
-        // });
-
-        // restaurante = new Restaurant("Test", 700);
-        // restaurante.addShipmentToInventory(lista2);
-
-        // listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg")]
-        // listaComida.map((comida)=> {
-        //     lista3.add(comida);
-        // });
-        // entree = new Entree('tests', lista3);
+        lista2 = new SortedListOfImmutables(null);
+        lista3 = new SortedListOfImmutables(null);
     });
-    it("3.Debe de retornar True", function() {
+    it("3.DEBE RETORNAR true", function() {
         restaurante = new Restaurant("Test", 700);
         listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
-        new Food("Egg", 47, 89, "Egg.jpg")]
+        new Food("Egg", 47, 89, "Egg.jpg")];
     
         listaComida.map((comida)=> {
             lista2.add(comida);
         });
-
-        //restaurante = new Restaurant("Test", 700);
         restaurante.addShipmentToInventory(lista2);
-
         listaComida2 = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg")]
-        listaComida2.map((comida)=> {
-            lista3.add(comida);
+        listaComida2.map((comida2)=> {
+            lista3.add(comida2);
         });
         entree = new Entree('tests', lista3);
-        //restaurante.addEntree(entree);
-        
         console.log(entree.getFoodList());
         expect(restaurante.placeOrder(entree)).toBeTruthy();
     });
-});
+    it("4.DEBE DE RETORNAR false", function() {
+        restaurante = new Restaurant("Test", 700);
+        listaComida = [new Food("Bacon", 89, 185, "Bacon.jpg"),new Food("Waffle", 178, 350, "Waffle.jpg"),
+        new Food("Egg", 47, 89, "Egg.jpg")];    
+        listaComida.map((comida)=> {
+            lista2.add(comida);
+        });
+        restaurante.addShipmentToInventory(lista2);
+        listaComida2 = [new Food("Pie", 195, 275, "Pie.jpg"),
+        new Food("Croissant", 106, 125, "Croissant.jpg")];
+        listaComida2.map((comida2)=> {
+            lista3.add(comida2);
+        });
+        entree = new Entree('tests', lista3);
+        console.log(entree.getFoodList());
+        expect(restaurante.placeOrder(entree)).toBeFalsy();
+    });
+    });
